538aadccade4056162634ac8c93490c4d96515a7
ABHIJEET <abhiatwork24@gmail.com>
2026-06-24 23:36:45 +0530

    adding new qu

---
+++ Stringss/CapitalFirst.java
@@ -0,0 +1,20 @@
+package Stringss;
+import java.util.*;
+
+public class CapitalFirst {
+    public static String Capitalize(String s){
+        String s1 = "";
+        for(int i= 0; i<s.length();i++){
+            if(s.charAt(i)!=' ' && (s.charAt(i-1)==' ' || i==0)){
+                s1+=Character.toUpperCase(s.charAt(i));
+            }
+            else{
+                s1+=s.charAt(i);
+            }
+        }
+        return s1.trim();
+    }
+    public static void main(String[] args) {
+        System.out.println(Capitalize("  I have a bigger one"));
+    }
+}
